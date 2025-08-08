c2f087ed113e1792275513a4faaf684c49f0bafe
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-08 10:20:12 +0900

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a25ex.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a25ex-user \
+    omni_a25ex-userdebug \
+    omni_a25ex-eng
---
+++ device.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a25ex
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_SHIPPING_API_LEVEL := 35
+PRODUCT_CHARACTERISTICS := phone
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
