99679b9efc179cf376ba3bebb498f9cf86e2aed6
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-10 05:35:06 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -28,7 +28,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
-    
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
