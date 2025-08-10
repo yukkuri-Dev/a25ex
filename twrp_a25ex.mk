cf24f113a989e12571f73062bfb77608e97b086e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-10 19:11:09 +0900

    Update twrp_a25ex.mk

--- twrp_a25ex.mk
+++ twrp_a25ex.mk
@@ -7,7 +7,6 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
